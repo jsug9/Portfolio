563f7c1080ffb5acdf78882db8eddb4ad3c270b5
Augusto <jesus_augga_99@icloud.com>
2022-05-23 15:42:11 -0500

    add folders to store documents

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
